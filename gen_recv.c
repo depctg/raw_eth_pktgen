bb029df45d3ead237e0db8cc1d38363d944b8fad
depctg <depctg@wuklab.io>
2022-04-04 19:18:48 -0700

    update example

--- gen_recv.c
+++ gen_recv.c
@@ -13,5 +13,43 @@ int main() {
 	printf("We are done\n");
 
 	return 0;
+
+        // remote1
+        const int size = 1024;
+        int a[size];
+        int sum = 0;
+        struct req *reqs = (struct req *)rbuf;
+        size_t num_reqs = RECV_BUF_SIZE / sizeof(struct req);
+        int * reps = (int *)sbuf;
+        while (1) {
+            recv(&reqs[0], sizeof(struct req));
+            // get datra
+            // Data copy!
+            reps[0] = a[reqs[0].index];
+            // sleep
+            // usleep();
+            send(&reps[0], sizeof(int));
+        }
+
+        // remote 2
+        const int size = 1024;
+        int a[size];
+        int sum = 0;
+        struct req *reqs = (struct req *)rbuf;
+        size_t num_reqs = RECV_BUF_SIZE / sizeof(struct req);
+        int * a = (int *)sbuf;
+        // init a
+        for (int i = 0; i < size; i++)
+                a[i] = 0;
+        while (1) {
+            recv(&reqs[0], sizeof(struct req));
+            // get datra
+            // Data copy!
+            // sleep
+            // usleep();
+            send(&a[req[0]->index], sizeof(int));
+        }
+
+
 }
 
--- gen_send.c
+++ gen_send.c
@@ -3,6 +3,15 @@
 #include "common.h"
 #include "packet.h"
 
+struct req {
+    enum {
+        REQ_WRITE,
+        REQ_READ,
+    };
+    int index;
+    int elsize;
+};
+
 int main() {
 	init(TRANS_TYPE_UDP);
 	steer();
@@ -20,5 +29,71 @@ int main() {
 	printf("We are done\n");
 
 	return 0;
+
+        // local
+        const int size = 1024;
+        int a[size];
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += a[i];
+        }
+
+        // remote
+        const int size = 1024;
+        int a[size];
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += a[i];
+        }
+
+        struct req *reqs = (struct req *)sbuf;
+        size_t num_reqs = SEND_BUF_SIZE / sizeof(struct req);
+        reqs[0].index = 0;
+        reqs[0].size = sizeof(int);
+        send(&req[0], sizeof(struct req));
+
+        const int size = 1024;
+        int a[size];
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            int ii = i % num_reqs;
+            // send req
+            reqs[ii].index = 0;
+            reqs[ii].size = sizeof(int);
+            send(&req[ii], sizeof(struct req));
+            // recv result
+            recv(&((int *)rbuf)[i], sizeof(int));
+            // get data from buffer
+            sum += ((int *)rbuf)[i];
+        }
+
+        // remote3 
+        reqs[0].index = 0;
+        reqs[0].size = sizeof(int);
+        send_aync(&req[0], sizeof(struct req));
+        int id = recv_aync(&((int *)rbuf)[i], sizeof(int));
+
+        for (int i = 0; i < size; i++) {
+            int ii = (i + 1) % num_reqs;
+            reqs[ii].index = 0;
+            reqs[ii].size = sizeof(int);
+            send_aync(&req[ii], sizeof(struct req));
+            // send req
+            // recv result
+            poll(id);
+            // get data from buffer
+            sum += ((int *)rbuf)[i];
+        }
+
+        // remote4
+        reqs[0].index = 0;
+        reqs[0].size = sizeof(int) * 16;
+        send_aync(&req[0], sizeof(struct req));
+        int id = recv_aync(&((int *)rbuf)[i], sizeof(int) * 16);
+
+
+
+
+
 }
 
